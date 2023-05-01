1ce402bf89de1d979b3c655bb28ddd4ef19473ce
Alexis Rondeau <alexis.rondeau@gmail.com>
2023-05-01 20:20:39 +0200

    Updates and makes .app bundle executable

--- EmbeddedPythonSpike/EmbeddedPythonSpikeApp.swift
+++ EmbeddedPythonSpike/EmbeddedPythonSpikeApp.swift
@@ -11,39 +11,80 @@ import PythonKit
 
 class EmbeddedPython: ObservableObject {
     init() {
+        
+        // Copy stuff over
+        copyBundleFolderToDocumentsFolder(bundleFolderName: "python-stdlib")
+
         // Initialize the Python runtime early in the app
-        if  let stdLibPath = Bundle.main.path(forResource: "python-stdlib", ofType: nil),
-            let libDynloadPath = Bundle.main.path(forResource: "python-stdlib/lib-dynload", ofType: nil)
-        {
-            setenv("PYTHONHOME", stdLibPath, 1)
-            setenv("PYTHONPATH", "\(stdLibPath):\(libDynloadPath)", 1)
-            Py_Initialize()
+        let stdLibPath = getDocumentsDirectory().appending(path: "python-stdlib")
+        let libDynloadPath = stdLibPath.appending(path: "lib-dynload")
+        
+        setenv("PYTHONHOME", stdLibPath.path(), 1)
+        setenv("PYTHONPATH", "\(stdLibPath):\(libDynloadPath)", 1)
+        Py_Initialize()
+    }
+        
+    func resourceURL(to path: String) -> URL? {
+        return URL(string: path, relativeTo: Bundle.main.resourceURL)
+    }
+    
+    func getDocumentsDirectory() -> URL {
+        // find all possible documents directories for this user
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+
+        // just send back the first one, which ought to be the only one
+        return paths[0]
+    }
+    
+    func copyBundleFolderToDocumentsFolder(bundleFolderName: String) {
+
+        let targetFolderURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first
+        let destURL = targetFolderURL!.appendingPathComponent(bundleFolderName)
+        
+        guard let sourceFolderURL = resourceURL(to: bundleFolderName) else  {
+            print("Could not find folder \(bundleFolderName) in Bundle")
+            return
+        }
+
+        let fileManager = FileManager.default
+        do {
+            try fileManager.copyItem(at: sourceFolderURL, to: destURL)
+        } catch {
+            print("Unable to copy folder \(bundleFolderName) from \(sourceFolderURL) to \(destURL)")
+            print("Error: \(error)")
         }
     }
     
     // Simple example of loading Python modules into the runtime
     // and accessing Python object members in Swift
-    func printPythonInfo() {
+    func getPythonInfo() -> String {
         let sys = Python.import("sys")
-        
-        print("Python Version: \(sys.version_info.major).\(sys.version_info.minor)")
-        print("Python Encoding: \(sys.getdefaultencoding().upper())")
-        print("Python Path: \(sys.path)")
-        
+        var output = ""
+        output += "Python Version: \(sys.version_info.major).\(sys.version_info.minor)"
+        output += "Python Encoding: \(sys.getdefaultencoding().upper())"
+        output += "Python Path: \(sys.path)"
         _ = Python.import("math") // verifies `lib-dynload` is found and signed successfully
+        
+        return output
     }
 }
 
 @main
 struct EmbeddedPythonSpikeApp: App {
     @StateObject var embeddedPython = EmbeddedPython()
+    @State var info = ""
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .onAppear {
-                    embeddedPython.printPythonInfo()
-                }
+            VStack {
+                Text(self.info)
+                    .multilineTextAlignment(.leading)
+                    .font(.body)
+            }
+            .padding()
+            .onAppear {
+                self.info = embeddedPython.getPythonInfo()
+            }
         }
     }
 }
